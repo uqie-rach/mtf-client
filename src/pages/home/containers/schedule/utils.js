e9695525cff44d2920c70ce29d9e182a029e5a6f
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2024-04-22 17:37:52 +0700

    feat: schedule

---
+++ src/pages/home/containers/schedule/utils.js
@@ -0,0 +1,38 @@
+export const timeline = [
+  {
+    date: "1 Mei - 17 Juni",
+    title: "Registrasi",
+    desc: "Registrasikan data tim-mu dan mulailah membangun aplikasi sesuai dengan ketentuan. Ini adalah tahap awal di mana tim-tim dapat mendaftarkan diri dan memulai perjalanan mereka menuju Maliki Tech Fest 2024.",
+  },
+  {
+    date: "20 Mei",
+    title: "Pembukaan gebyar Maliki Tech Fest 2024",
+    desc: "Acara pembukaan resmi Maliki Tech Fest 2024 akan diadakan pada tanggal 20 Mei. Bergabunglah dalam acara pembukaan ini untuk menyambut festival teknologi yang menjanjikan ini dengan semangat dan antusiasme.",
+  },
+  {
+    date: "25 Mei, 1 & 8 Juni",
+    title: "Sesi Mentoring bersama komunitas",
+    desc: "Jadwalkan tanggal-tanggal ini untuk sesi mentoring yang berharga bersama komunitas. Dapatkan wawasan dan bimbingan dari para ahli teknologi dalam memperkuat dan mengembangkan proyek-proyek Anda.",
+  },
+  {
+    date: "13 Mei - 17 Juni",
+    title: "Pengumpulan proyek",
+    desc: "Waktu untuk mengumpulkan hasil kerja keras Anda! Pastikan untuk menyelesaikan proyek Anda sesuai dengan tenggat waktu yang ditentukan agar dapat diikutsertakan dalam penilaian.",
+  },
+  {
+    date: "18 - 21 Juni",
+    title: "Penilaian proyek",
+    desc: "Para juri akan menilai proyek-proyek yang telah diajukan selama periode pengumpulan. Pastikan proyek Anda memenuhi kriteria penilaian dan siap untuk dinilai secara obyektif.",
+  },
+  {
+    date: "22 Juni",
+    title: "Seminar dan pengumuman pemenang",
+    desc: "Selamat datang di acara penutupan Maliki Tech Fest 2024! Saksikan seminar inspiratif dan pengumuman pemenang di acara penutupan ini. Jadikan momen ini sebagai kesempatan untuk merayakan prestasi dan memperoleh wawasan tambahan dari para ahli industri.",
+  },
+];
+
+export const headerData = {
+  label: "schedule",
+  title: "schedule and agenda",
+  desc: "Pastikan anda tidak melewatkannya",
+};
